20ec458c3841bea524729e1f8f63ef2a3088cd2e
Mayank <msdurve@gmail.com>
2020-04-11 10:58:38 +0530

    Modified on 11-Apr-2020 for New Project Setup ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
   // styleUrls: ['./app.component.css']
   styles:[`
   h1{
@@ -12,10 +11,5 @@ import { Component } from '@angular/core';
   `]
 })
 export class AppComponent { 
-=======
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
->>>>>>> 271ee60a4025704915e619bf60ce40e93bcffdc2
   title = 'my-first-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms'
-
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-
-@NgModule({
-  declarations: [
-    AppComponent ,
-    ServerComponent,
-    ServersComponent  
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-
-=======
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -27,7 +9,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
->>>>>>> 271ee60a4025704915e619bf60ce40e93bcffdc2
   ],
   providers: [],
   bootstrap: [AppComponent]
